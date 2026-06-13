5666fba0708954867da67992de96ce8556a13123
Darshanj32 <darshanjagashetty@gmail.com>
2026-06-13 12:04:06 +0530

    feat:connect mongodb database to backend

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zw6hky5.mongodb.net/?retryWrites=true&w=majority`;
+const url = "mongodb+srv://chatapphackthon:DJkumar143@cluster0.95tmhya.mongodb.net/chatapp?retryWrites=true&w=majority";
 
 mongoose.connect(url, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
+})
+.then(() => console.log('Connected to DB'))
+.catch((e) => console.log('Error', e));
